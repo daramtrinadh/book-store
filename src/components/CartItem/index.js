d42dfc214a628a8691023ece03df8189a4f8ab6f
Daram Trinadh Reddy <daramtrinadh@gmail.com>
2024-06-16 14:41:17 +0530

    updated price type

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,13 +1,11 @@
 const CartItem=(prop)=>{
     const {cartItemDetail}=prop
     const { image, price, title } = cartItemDetail;
-    const inr = price.slice(1) * 83.55;
-  const inrFinal = Math.ceil(inr * 100) / 100;
     return(
         <li className="cart-item-detail">
             <img src={image} alt={title}/>
             <p>{title}</p>
-            <p>{inrFinal.slice(1)}</p>
+            <p>{price}</p>
         </li>
 
     )
